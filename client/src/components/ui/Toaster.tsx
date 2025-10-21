9d83a6bfcb4d979640257d4075ccf17ff0f07b07
djw8889-sys <djw8889@gmail.com>
2025-10-21 05:33:01 +0000

    fix: restore missing Toaster.tsx component

---
+++ client/src/components/ui/Toaster.tsx
@@ -0,0 +1,10 @@
+// client/src/components/ui/Toaster.tsx
+import { Toaster as Sonner } from "sonner";
+
+/**
+ * Shadcn UI 기반 Toast 컴포넌트 Wrapper
+ * - 프로젝트 전역에서 Toaster를 사용하기 위함
+ */
+export function Toaster() {
+  return <Sonner />;
+}
